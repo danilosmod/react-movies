7ffff33b0fd632a6270093192b71c6c8c705f0d1
danilosmod <danilo.oldschool@gmail.com>
2022-05-19 20:53:16 -0300

    Setup inicial

---
+++ src/Pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div>
+        <h1>Home React Movies</h1>
+        <p>Página inicial do site</p>
+        </div>
+    )
+}
+
+export default Home;
